1bedb54a25d742abfef75781e8482cb87b1a98d9
luisfuentech <luisfuentes.au@gmail.com>
2021-10-24 17:08:24 -0500

    Feat. Add timing rules, serial, parser text to morse

--- Morse.cpp
+++ Morse.cpp
@@ -11,26 +11,238 @@ This says that the function is part of the Morse class
 
 Constructor
 */
+
+Morse::Morse() {}
+
 Morse::Morse(int pin)
 {
     pinMode(pin, OUTPUT);
     _pin = pin;
+    _text = "";
 }
 
-// Delaclare dot function
+void Morse::begin(long baudRate, HardwareSerial *_serial)
+{
+    serial = _serial;
+    serial->begin(baudRate);
+    print("Initializing Serial Port...");
+}
+
+void Morse::begin(long baudRate)
+{
+    serial = &Serial;
+    serial->begin(baudRate);
+    print("Initializing Serial Port...\n");
+}
+
+void Morse::print(String text)
+{
+    serial->print(text);
+}
+
 void Morse::dot()
 {
     digitalWrite(_pin, HIGH);
-    delay(DOT_LENGTH);
+    delay(DOT_TIMING);
+
+    if (serial)
+    {
+        print(".");
+    }
+
     digitalWrite(_pin, LOW);
     delay(DELAY);
 }
 
-//Declare dash function
 void Morse::dash()
 {
     digitalWrite(_pin, HIGH);
-    delay(DASH_LENGTH);
+    delay(DASH_TIMING);
+
+    if (serial)
+    {
+        print("-");
+    }
+
     digitalWrite(_pin, LOW);
     delay(DELAY);
+}
+
+void Morse::space()
+{
+    digitalWrite(_pin, HIGH);
+    delay(SPACE_TIMING);
+
+    if (serial)
+    {
+        print("/");
+    }
+
+    digitalWrite(_pin, LOW);
+    delay(DELAY);
+}
+
+void Morse::processMorse(String text)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        char character = text.charAt(i);
+
+        switch (character)
+        {
+        case '.':
+            dot();
+            break;
+        case '-':
+            dash();
+            break;
+        case '/':
+        case '|':
+            space();
+            break;
+        }
+    }
+    serial->print(" ");
+}
+
+void Morse::parse(String text)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        text.toLowerCase();
+        char character = text.charAt(i);
+
+        switch (character)
+        {
+        case 'a':
+            processMorse(".-");
+            break;
+        case 'b':
+            processMorse("-...");
+            break;
+        case 'c':
+            processMorse("-.-.");
+            break;
+        case 'd':
+            processMorse("-..");
+            break;
+        case 'e':
+            processMorse(".");
+            break;
+        case 'f':
+            processMorse("..-.");
+            break;
+        case 'g':
+            processMorse("--.");
+            break;
+        case 'h':
+            processMorse("....");
+            break;
+        case 'i':
+            processMorse("..");
+            break;
+        case 'j':
+            processMorse(".---");
+            break;
+        case 'k':
+            processMorse("-.-");
+            break;
+        case 'l':
+            processMorse(".-..");
+            break;
+        case 'm':
+            processMorse("--");
+            break;
+        case 'n':
+            processMorse("-.");
+            break;
+        case 'o':
+            processMorse("---");
+            break;
+        case 'p':
+            processMorse(".--.");
+            break;
+        case 'q':
+            processMorse("--.-");
+            break;
+        case 'r':
+            processMorse(".-.");
+            break;
+        case 's':
+            processMorse("...");
+            break;
+        case 't':
+            processMorse("-");
+            break;
+        case 'u':
+            processMorse("..-");
+            break;
+        case 'v':
+            processMorse("...-");
+            break;
+        case 'w':
+            processMorse(".--");
+            break;
+        case 'x':
+            processMorse("-..-");
+            break;
+        case 'y':
+            processMorse("-.--");
+            break;
+        case 'z':
+            processMorse("--..");
+            break;
+        case ' ':
+            processMorse("/");
+            break;
+        case '0':
+            processMorse("-----");
+            break;
+        case '1':
+            processMorse(".----");
+            break;
+        case '2':
+            processMorse("..---");
+            break;
+        case '3':
+            processMorse("...--");
+            break;
+        case '4':
+            processMorse("....-");
+            break;
+        case '5':
+            processMorse(".....");
+            break;
+        case '6':
+            processMorse("-....");
+            break;
+        case '7':
+            processMorse("--...");
+            break;
+        case '8':
+            processMorse("---..");
+            break;
+        case '9':
+            processMorse("----.");
+            break;
+        case '.':
+            processMorse(".-.-.-");
+            break;
+        case '?':
+            processMorse("..--..");
+            break;
+        case ',':
+            processMorse("--..--");
+            break;
+        case '=':
+            processMorse("-...-");
+            break;
+        default:
+            break;
+        }
+
+        delay(LETTER_TIMING);
+    }
+
+    serial->print("\n");
 }
--- Morse.h
+++ Morse.h
@@ -7,31 +7,40 @@ file has definitions for the library: basically a listing of everything that's i
 
 #include "Arduino.h"
 
+/*
+There are rules to help people distinguish dots from dashes in Morse code.
+
+    1. The length of a dot is 1 time unit.
+    2. A dash is 3 time units.
+    3. The space between symbols (dots and dashes) of the same letter is 1 time unit.
+    4. The space between letters is 3 time units.
+    5. The space between words is 7 time units
+*/
+
+#define DOT_TIMING 45
+#define DASH_TIMING DOT_TIMING * 3
+#define SPACE_TIMING DOT_TIMING * 7
+#define DELAY DOT_TIMING
+#define LETTER_TIMING DOT_TIMING * 3
 class Morse
 {
 public:
+    Morse();
     Morse(int pin);
+    HardwareSerial *serial;
     void dot();
     void dash();
-    static const int DOT_LENGTH = 250;
-    static const int DASH_LENGTH = 1000;
-    static const int DELAY = 250;
+    void space();
+    void begin(long baudRate);
+    void begin(long baudRate, HardwareSerial *_serial);
+    void print(String text);
+    void parse(String text);
 
 private:
+    void processMorse(String text);
     int _pin;
+    long _baudRate;
+    String _text;
 };
 
 #endif
-
-/*
-This code is necessary:
-#ifndef {fileName}_h
-#define {fileName}_h
-
-#include "Arduino.h"
-
-The code go here...
-
- #endif
-
-*/
